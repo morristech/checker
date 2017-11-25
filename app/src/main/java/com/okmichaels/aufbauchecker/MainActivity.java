4fab501fee40a1a66a6b0532978145ec340b862d
idoqo <okokomichaels@outlook.com>
2017-11-25 13:57:16 +0100

    added scaffold for result activity

--- app/src/main/java/com/okmichaels/aufbauchecker/MainActivity.java
+++ app/src/main/java/com/okmichaels/aufbauchecker/MainActivity.java
@@ -1,13 +1,31 @@
 package com.okmichaels.aufbauchecker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button checkResultButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkResultButton = (Button) findViewById(R.id.check_result);
+        initUI();
+    }
+
+    private void initUI() {
+        checkResultButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
