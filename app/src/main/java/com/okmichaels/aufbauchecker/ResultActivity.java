3365df36f60c7e402fcd659b0fbd28ad41465aa4
idoqo <okokomichaels@outlook.com>
2018-01-29 02:07:05 +0100

    added content model, updated ResultActivity to use content :)

--- app/src/main/java/com/okmichaels/aufbauchecker/ResultActivity.java
+++ app/src/main/java/com/okmichaels/aufbauchecker/ResultActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.okmichaels.aufbauchecker.adapters.ResultAdapter;
+import com.okmichaels.aufbauchecker.model.Content;
 import com.okmichaels.aufbauchecker.model.Grade;
 import com.okmichaels.aufbauchecker.model.Response;
 
@@ -27,6 +29,9 @@ public class ResultActivity extends AppCompatActivity {
     private ResultAdapter resultAdapter;
     private ArrayList<Grade> grades;
 
+    private TextView tvCandName; private TextView tvCandNumber;
+    private TextView tvCenterName;
+
     private static String BASE_URL = "http://allamihomes.com";
 
     @Override
@@ -37,6 +42,8 @@ public class ResultActivity extends AppCompatActivity {
         resultRecyclerView = findViewById(R.id.recycler_view_result);
         RecyclerView.LayoutManager lm = new LinearLayoutManager(this);
         resultRecyclerView.setLayoutManager(lm);
+
+        tvCandName = findViewById(R.id.tv_candidate_name);
 
         loadData();
     }
@@ -61,10 +68,35 @@ public class ResultActivity extends AppCompatActivity {
 
     private void handleResponse(List<Response> responses) {
         Toast.makeText(this, "gotten response", Toast.LENGTH_SHORT).show();
+
+        //get the first (and only) item in responses
+        Response response = responses.get(0);
+        if (response.getHttp_code().equals("200")) {
+            parseResult(response.getContent());
+        } else {
+            handleResultError();
+        }
     }
 
     private void handleError(Throwable error) {
         Log.d("ResultActivity", error.getLocalizedMessage());
         Toast.makeText(this, error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+    }
+
+    private void parseResult(Content content) {
+        tvCandName.setText(content.getCandidate_number());
+        /*resultAdapter = new ResultAdapter(content.getGrades(), this.getApplicationContext());
+        resultRecyclerView.setAdapter(resultAdapter);*/
+        if (content.getGrades().isEmpty() || content.getGrades() == null) {
+            Log.d("ContentGrade", "nothing to see here...keep moving");
+        } else {
+            for (Grade grd : content.getGrades()) {
+                Log.d("ContentGrade", grd.getSubject());
+            }
+        }
+    }
+
+    private void handleResultError() {
+
     }
 }
--- app/src/main/java/com/okmichaels/aufbauchecker/model/Content.java
+++ app/src/main/java/com/okmichaels/aufbauchecker/model/Content.java
@@ -1,10 +1,56 @@
 package com.okmichaels.aufbauchecker.model;
 
 
+import java.util.ArrayList;
+
 public class Content {
+    //generic attrs
     private String title;
+
+    //success attrs
+    private String candidate_number;
+    private String candidate_name;
+    private String exam_type;
+    private String center;
+    private ArrayList<Grade> grades;
+
+    //failure attrs
+    private String error_message;
+    private String error_title;
 
     public String getTitle() {
         return this.title;
+    }
+
+    public String getCandidate_number() {
+        return candidate_number;
+    }
+
+    public String getCandidate_name() {
+        return candidate_name;
+    }
+
+    public String getExam_type() {
+        return exam_type;
+    }
+
+    public String getCenter() {
+        return center;
+    }
+
+    public ArrayList<Grade> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(ArrayList<Grade> grd) {
+        this.grades = grd;
+    }
+
+    public String getError_message() {
+        return error_message;
+    }
+
+    public String getError_title() {
+        return error_title;
     }
 }
